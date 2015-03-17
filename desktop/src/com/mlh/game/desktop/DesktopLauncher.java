fd754b35b815c6bf22ce3ed8d9f6f943e45a501d
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-03-17 13:22:40 +0900

    Initial commit

---
+++ desktop/src/com/mlh/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mlh.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mlh.game.HMSDreadnought;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HMSDreadnought(), config);
+	}
+}
